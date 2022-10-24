b7063de9ed8100d27f2f92879426d5518a2e0104
Ivan Alexander Gonzalez Gonzalez <hal308366@gmail.com>
2022-10-24 13:51:04 -0700

    [ADD] PUT, PATCH, DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -38,4 +38,33 @@ router.post('/', (req, res) => {
   });
 });
 
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update all',
+    data: body,
+    id,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  });
+});
+
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'deleted',
+    id,
+  });
+});
+
 module.exports = router;
